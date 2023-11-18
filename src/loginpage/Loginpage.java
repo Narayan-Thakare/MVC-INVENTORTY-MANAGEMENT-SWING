ef2c884fd9d30ff03eed14400905ce25d062e44c
Narayan Thakare <narayan111thakare@gmail.com>
2023-11-18 17:45:46 +0530

    second commit

--- src/loginpage/Loginpage.java
+++ src/loginpage/Loginpage.java
@@ -22,7 +22,7 @@ public class Loginpage {
                 @Override
                 protected void paintComponent(Graphics g) {
                     super.paintComponent(g);
-                    Image image = new ImageIcon("D:\\HTML\\code 897629.jpg").getImage();
+                    Image image = new ImageIcon("D:\\Asterics Website\\assets\\img\\camera-bg.jpg").getImage();
                     g.drawImage(image, 0, 0, getWidth(), getHeight(), this);
                 }
             };
@@ -57,15 +57,13 @@ public class Loginpage {
             loginButton.setBounds(350, 250, 200, 40);
             loginButton.setForeground(Color .magenta);
 
-            // Add ActionListener to the login button
             loginButton.addActionListener(new ActionListener() {
                 @Override
                 public void actionPerformed(ActionEvent e) {
                     String username = tx.getText();
                     String password = new String(passwordField.getPassword());
 
-                    // Check if the username and password are correct
-                    if (username.equals("asd") && password.equals("123")) {
+                    if (username.equals("narayan thakare") && password.equals("password dall")) {
                     	
                         JOptionPane.showMessageDialog(frame, "Login Successful!");
                        
